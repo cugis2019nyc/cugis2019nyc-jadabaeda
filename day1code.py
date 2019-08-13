1e72f5c7a8b82b3fabecb95f694516761444ab8c
jadabaeda <54039928+jadabaeda@users.noreply.github.com>
2019-08-13 15:00:18 -0400

    Add files via upload

---
+++ day1code.py
@@ -0,0 +1,89 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+print("hello world")
+print("my name is jada")
+print("1226")
+
+5*2
+5/2
+5-2
+5**2
+8/9*3
+
+def add(a,b):
+    add = a+b
+    print(add)
+
+add(2,3)
+
+print("the sum of 23,70,and 97 is")
+def add(a,b,c):
+    add = a+b+c
+    print("the sum is ", add)
+    
+    
+add(23,70,97)
+
+def aqua(a,b,c):
+    aqua = a*b*c
+    print("the product of" ,a,b, "and" ,c, "is" ,aqua)
+    
+aqua(5,5,5)
+
+1/2*2*1
+1/2*11*19
+
+def area(b,h):
+    area = 1/2*b*h
+    print(area)
+    print("the area of a triangle with base", b, "and height", h, "is volume", area)
+area(11, 19)
+
+print("cwhite chocolate")
+print("milk chocolate")
+
+chocolate1="milk"
+chocolate2="dark"
+chocolate3="white"
+
+milk=5
+dark=6
+white=8
+
+milk = milk+3
+milk
+
+milk = milk-3
+milk
+
+print(milk+2)
+
+
+import math
+dir(math)
+
+math.factorial(7)
+math.sqrt(350)
+math.exp(1)
+math.pow(2.718281828459045,2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
